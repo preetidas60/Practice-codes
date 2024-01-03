c1161047858e34d01483ef7292848abd72f6b94b
preetidas60 <dasp69833@gmail.com>
2024-01-03 20:50:40 +0530

    added 6th practice code

---
+++ src/Practice6.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        // Fill in code
+        println(name)
+        println(age)
+        print("Likes to $hobby. ")
+        if(referrer != null){
+            print("Has a referrer named ${referrer.name},")
+            if(referrer.hobby != null){
+                print("who likes to ${referrer.hobby}.")
+            }
+            else{
+                print(".")
+            }
+        }
+        else{
+            println("Doesn't have a referrer.\n")
+        }
+    }
+}
