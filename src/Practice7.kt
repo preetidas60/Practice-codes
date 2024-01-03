6b48d0131cb64cf594ff1e861b989902e5026188
preetidas60 <dasp69833@gmail.com>
2024-01-03 21:44:42 +0530

    Added 7th practice code

---
+++ src/Practice7.kt
@@ -0,0 +1,36 @@
+open class Phone(var isScreenLightOn: Boolean ){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var folded : Boolean) : Phone(isScreenLightOn = folded) {
+    override fun switchOn() {
+        if (!folded) {
+            isScreenLightOn = true
+        }
+    }
+
+    fun fold(){
+       isScreenLightOn = true
+    }
+    fun unFold(){
+        isScreenLightOn = false
+    }
+}
+
+fun main(){
+    val phoneStatus = FoldablePhone(false)
+    phoneStatus.checkPhoneScreenLight()
+    phoneStatus.switchOn()
+    phoneStatus.checkPhoneScreenLight()
+}
